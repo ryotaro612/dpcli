3e3d14332fee60f2b2c7ed1cb01bfd6b93fefe0f
Ryotaro <ryotaro612.ooo@gmail.com>
2024-07-16 01:05:10 +0900

    temp

---
+++ internal/aws.go
@@ -0,0 +1,18 @@
+package internal
+
+import (
+	"context"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/secretsmanager"
+)
+
+func Load(ctx context.Context) (*secretsmanager.Client, error) {
+	cfg, err := config.LoadDefaultConfig(ctx, config.WithRegion("ap-northeast-1"))
+	var svc *secretsmanager.Client
+	if err != nil {
+		return svc, err
+	}
+	svc = secretsmanager.NewFromConfig(cfg)
+	return svc, nil
+}
